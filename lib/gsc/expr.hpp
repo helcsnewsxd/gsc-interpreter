53e0f307e1860e84c7c84222f5da66b69af7d661
helcsnewsxd <emanuelherrador2@gmail.com>
2025-05-16 16:23:10 +0000

    Changed Visitor class name to ExprVisitor to avoid conflict with the next StmtVisitor class

--- lib/gsc/expr.hpp
+++ lib/gsc/expr.hpp
@@ -9,18 +9,18 @@ class Grouping;
 class Literal;
 class Unary;
 
-class Visitor {
+class ExprVisitor {
 public:
   virtual std::any visitBinaryExpr(std::shared_ptr<Binary> expr) = 0;
   virtual std::any visitGroupingExpr(std::shared_ptr<Grouping> expr) = 0;
   virtual std::any visitLiteralExpr(std::shared_ptr<Literal> expr) = 0;
   virtual std::any visitUnaryExpr(std::shared_ptr<Unary> expr) = 0;
 
-  virtual ~Visitor() = default;
+  virtual ~ExprVisitor() = default;
 };
 
 class Expr {
-  virtual std::any accept(Visitor &visitor) = 0;
+  virtual std::any accept(ExprVisitor &visitor) = 0;
 };
 
 class Binary : public Expr, public std::enable_shared_from_this<Binary> {
@@ -33,7 +33,7 @@ public:
   Binary(std::shared_ptr<Expr> left, Token op, std::shared_ptr<Expr> right)
       : left(std::move(left)), op(std::move(op)), right(std::move(right)) {}
 
-  std::any accept(Visitor &visitor) override {
+  std::any accept(ExprVisitor &visitor) override {
     return visitor.visitBinaryExpr(shared_from_this());
   }
 
@@ -50,7 +50,7 @@ public:
   Grouping(std::shared_ptr<Expr> expression)
       : expression(std::move(expression)) {}
 
-  std::any accept(Visitor &visitor) override {
+  std::any accept(ExprVisitor &visitor) override {
     return visitor.visitGroupingExpr(shared_from_this());
   }
 
@@ -64,7 +64,7 @@ private:
 public:
   Literal(std::any value) : value(std::move(value)) {}
 
-  std::any accept(Visitor &visitor) override {
+  std::any accept(ExprVisitor &visitor) override {
     return visitor.visitLiteralExpr(shared_from_this());
   }
 
@@ -80,7 +80,7 @@ public:
   Unary(Token op, std::shared_ptr<Expr> right)
       : op(std::move(op)), right(std::move(right)) {}
 
-  std::any accept(Visitor &visitor) override {
+  std::any accept(ExprVisitor &visitor) override {
     return visitor.visitUnaryExpr(shared_from_this());
   }
 
